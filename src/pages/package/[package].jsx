b6c34c40ebcc7a08d5a7075727c4e36e1ff5daa0
vivek <Vivekchauhan1181@gmail.com>
2024-07-15 14:45:17 +0530

    all bugs complete

--- src/pages/package/[package].jsx
+++ src/pages/package/[package].jsx
@@ -1,17 +1,14 @@
-// Import necessary styles and components
+import dynamic from 'next/dynamic';
 import "../../app/globals.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
-import Package1 from "@/components/ItineraryDetail/Departure&Booking/package1";
-import { AppProvider } from "@/components/admin/context/Package/AddGuest";
+const Package1 = dynamic(() => import('@/components/ItineraryDetail/Departure&Booking/package1'), { ssr: false });
+const AppProvider = dynamic(() => import('@/components/admin/context/Package/AddGuest'), { ssr: false });
 
 export default function TourPackage() {
-
   return (
     <AppProvider>
-      {/* <Package1 /> */}
-      <div>HELLO WORLD</div>
+      <Package1 />
+      {/* <div>HELLO WORLD</div> */}
     </AppProvider>
   );
 }
