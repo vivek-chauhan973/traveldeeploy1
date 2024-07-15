31174e9e37b04247d1aa1aa800b191c9322ea1ea
vivek <Vivekchauhan1181@gmail.com>
2024-07-15 16:34:11 +0530

    code deploy uri

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,22 +1,33 @@
 import mongoose from 'mongoose';
 import { Db, MongoClient } from 'mongodb';
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://vermapradhumn3:pradhumn@cluster0.xo2ekjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const MONGODB_URI = "mongodb+srv://vermapradhumn3:pradhumn@cluster0.xo2ekjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Global is used here to maintain a cached connection across hot reloads in development. In production, a new connection is created for each deployment.
+let cached = global.mongoose;
 
-const connectToDatabase = async () => {
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
-    try {
-        if (mongoose.connection.readyState === 0) {
-            await mongoose.connect(MONGODB_URI, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            })
-            console.log('Connected to MongoDB');
-        }
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-    }
+async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 export default connectToDatabase;
