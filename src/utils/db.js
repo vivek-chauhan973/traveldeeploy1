6ff39292985e210f3cd21374137060def2572fdb
vivek <Vivekchauhan1181@gmail.com>
2024-07-22 01:51:37 +0530

    changes

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const connectToDatabase = async () => {
     try {
         const conn=await mongoose.connect(process.env.MONGODB_URI);
-        return  conn;
+        return conn;
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
     }
