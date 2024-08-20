727147399239fb336b1c158ac596fce661214596
rakesh4stepsdigital <rakesh@4stepsdigital.com>
2024-08-20 16:02:58 +0530

    Update package tour  table

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div>
+            <div class="gooey">
+                <span class="dot"></span>
+                <div class="dots">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loading
