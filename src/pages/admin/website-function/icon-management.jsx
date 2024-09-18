71f4329347381756bf289557e8ca3ae795cab3ed
Pradh123 <vermapradhumn3@gmail.com>
2024-09-18 12:11:51 +0530

    pradhumn admin pannel

--- src/pages/admin/website-function/icon-management.jsx
+++ src/pages/admin/website-function/icon-management.jsx
@@ -1,24 +1,24 @@
 import { AppProvider } from "@/components/admin/context/Package/AddGuest";
 import Layout from "@/components/admin/Layout";
-import Image from 'next/image';
-import { useState,useEffect } from "react";
+import Image from "next/image";
+import { useState, useEffect } from "react";
 export default function IconManagement() {
   const [file, setFile] = useState(null);
   const [preview, setPreview] = useState(null);
-  const [title, setTitle] = useState('');
-  const [alt, setAlt] = useState('');
+  const [title, setTitle] = useState("");
+  const [alt, setAlt] = useState("");
   const [isUpdating, setIsUpdating] = useState(false);
   const [selectedImageId, setSelectedImageId] = useState(null);
-  const [imageData,setImageData]=useState([]);
+  const [imageData, setImageData] = useState([]);
 
   // Function to fetch existing image
   async function fetchImage() {
     try {
-      const res = await fetch('/api/icon/icon1');
+      const res = await fetch("/api/icon/icon1");
       const data = await res.json();
-      setImageData(data?.data||[]);
+      setImageData(data?.data || []);
     } catch (error) {
-      console.error('Error fetching image:', error);
+      console.error("Error fetching image:", error);
     }
   }
 
@@ -37,35 +37,34 @@ export default function IconManagement() {
   // Function to handle image upload or update
   async function handleUpload() {
     if (!file && !isUpdating) {
-      alert('Please select a file to upload.');
+      alert("Please select a file to upload.");
       return;
     }
 
     const formData = new FormData();
     if (file) {
-      formData.append('file', file);
+      formData.append("file", file);
     }
-    formData.append('title', title);
-    formData.append('alt', alt);
+    formData.append("title", title);
+    formData.append("alt", alt);
     if (selectedImageId) {
-      formData.append('id', selectedImageId);
+      formData.append("id", selectedImageId);
     }
 
     try {
-      const res = await fetch('/api/icon/icon1', {
-        method: 'POST',
+      const res = await fetch("/api/icon/icon1", {
+        method: "POST",
         body: formData,
       });
       if (res.ok) {
         setFile(null);
         setPreview(null);
-        setTitle('');
-        setAlt('');
+        setTitle("");
+        setAlt("");
         setIsUpdating(false);
         setSelectedImageId(null);
         alert(`File uploaded successfully`);
-        // Optionally, reset form fields or update state after successful upload
-        fetchImage(); // Fetch updated image
+        fetchImage(); 
       } else {
         alert(`File upload failed`);
       }
@@ -79,34 +78,44 @@ export default function IconManagement() {
   async function handleRemove(selectedImageId) {
     try {
       const res = await fetch(`/api/icon/icon1?id=${selectedImageId}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
 
       if (res.ok) {
-        const data=imageData?.filter(item=>item?._id!==selectedImageId);
+        const data = imageData?.filter((item) => item?._id !== selectedImageId);
         setImageData(data);
-        alert('Image removed successfully');
+        alert("Image removed successfully");
         // Reset state after successful removal
       } else {
-        alert('Failed to remove image');
+        alert("Failed to remove image");
       }
     } catch (error) {
-      console.error('Error removing image:', error);
-      alert('Failed to remove image');
+      console.error("Error removing image:", error);
+      alert("Failed to remove image");
     }
   }
-  console.log("preview the image",preview)
+  // console.log("preview the image", preview);
   return (
     <AppProvider>
       <Layout>
         <p className="md:text-[28px] text-xl font-semibold">Logo Management</p>
         <div className="md:my-10 my-7 bg-white px-5 py-4 rounded-md">
-          <p className="md:text-[20px] text-base font-semibold">Bizare Expenditure Main Website</p>
+          <p className="md:text-[20px] text-base font-semibold">
+            Bizare Expenditure Main Website
+          </p>
           <div>
             <div className="flex md:flex-row flex-col md:gap-10 gap-5 md:my-10 my-5 items-center xl:pl-5">
               <input type="file" className="mb-4" onChange={handleChange} />
               <div>
-                {preview && <Image className="md:w-36 w-auto h-auto shadow-md mb-4" src={preview} alt="Preview" width={150} height={200} />}
+                {preview && (
+                  <Image
+                    className="md:w-36 w-auto h-auto shadow-md mb-4"
+                    src={preview}
+                    alt="Preview"
+                    width={150}
+                    height={200}
+                  />
+                )}
                 <div>
                   <p>Name of Icon</p>
                   <input
@@ -128,38 +137,43 @@ export default function IconManagement() {
               </div>
             </div>
             <div className="flex md:flex-row flex-col md:gap-5 gap-3">
-              <button className="bg-[#0d6efd] text-white px-3 py-2 w-full md:w-auto rounded-sm" onClick={handleUpload}>
-              Upload Image
+              <button
+                className="bg-[#0d6efd] text-white px-3 py-2 w-full md:w-auto rounded-sm"
+                onClick={handleUpload}
+              >
+                Upload Image
               </button>
             </div>
           </div>
           <div className="mt-10">
-  {imageData?.map((item) => (
-    <div key={item?._id} className="flex flex-col md:flex-row gap-10 items-center md:items-start mb-5">
-      <div className="flex items-center justify-center">
-      <Image
-        className="w-36 h-auto shadow-md"
-        src={item?.path}
-        alt="Preview"
-        width={150}
-        height={200}
-      />
-      </div>
-      <div className="flex items-center justify-center">
-        <p className="text-lg font-semibold">{item?.title}</p>
-      </div>
-      <div>
-      <button
-          className="bg-red-600 flex items-center justify-center text-white px-3 py-2 rounded-sm hover:bg-red-700 transition-all"
-          onClick={()=>handleRemove(item?._id)}
-        >
-          Remove
-        </button>
-      </div>
-    </div>
-  ))}
-</div>
-
+            {imageData?.map((item) => (
+              <div
+                key={item?._id}
+                className="flex flex-col md:flex-row gap-10 items-center md:items-start mb-5"
+              >
+                <div className="flex items-center justify-center">
+                  <Image
+                    className="w-36 h-auto shadow-md"
+                    src={item?.path}
+                    alt="Preview"
+                    width={150}
+                    height={200}
+                  />
+                </div>
+                <div className="flex items-center justify-center">
+                  <p className="text-lg font-semibold">{item?.title}</p>
+                </div>
+                <div>
+                  <button
+                    className="bg-red-600 flex items-center justify-center text-white px-3 py-2 rounded-sm hover:bg-red-700 transition-all"
+                    onClick={() => handleRemove(item?._id)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </Layout>
     </AppProvider>
