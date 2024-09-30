d6b57ad7a493e343018ef903e66e799389e575a5
Pradh123 <vermapradhumn3@gmail.com>
2024-09-30 12:07:22 +0530

    pradhumn is changed the code of [package file]

--- src/pages/package/[package].jsx
+++ src/pages/package/[package].jsx
@@ -3,19 +3,19 @@ import { AppProvider } from "@/components/admin/context/Package/AddGuest";
 import Package1 from "@/components/ItineraryDetail/Departure&Booking/package1";
 // import FAQSchema from "@/components/seo/FAQSchema";
 import Head from "next/head";
-// export async function getServerSideProps(context) {
-  // const packageUrl = context.params.package;
-  // const newPackageUrl = packageUrl.replace("-tour-package", " ");
-  //  const res = await fetch(
-  //   `http://89.116.34.248:3000/api/public/package/${newPackageUrl}`
-  // );
-  // const data = await res.json();
-  // return {
-  //   props: {
-  //     data,
-  //   },
-  // };
-// }
+export async function getServerSideProps(context) {
+  const packageUrl = context.params.package;
+  const newPackageUrl = packageUrl.replace("-tour-package", " ");
+   const res = await fetch(
+    `http://89.116.34.248:3000/api/public/package/${newPackageUrl}`
+  );
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
+}
 export default function TourPackage() {
   return (
     <>
