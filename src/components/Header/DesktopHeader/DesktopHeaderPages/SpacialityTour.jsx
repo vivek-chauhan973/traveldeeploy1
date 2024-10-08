d14ef101c2e655f5f477263d8fc3b3ec73fc364f
Pradh123 <vermapradhumn3@gmail.com>
2024-10-08 17:47:58 +0530

    all changes done by pradhumn date 08-10-2024 tuesday

--- src/components/Header/DesktopHeader/DesktopHeaderPages/SpacialityTour.jsx
+++ src/components/Header/DesktopHeader/DesktopHeaderPages/SpacialityTour.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+const fetchCatagories=async ()=>{
+  const categoriesList = await fetch('/api/package-setting/category/get-categories');
+  return await categoriesList.json();
+}
 const SpacialityTour = () => {
+
+  const [categories,setCatagories]=useState([]);
+
+  useEffect(()=>{
+    fetchCatagories().then(res=>setCatagories(res?.data||[]));
+  },[])
+
+  console.log("categories-----> ",categories);
   return (
     <div className=" flex ml-52 flex-col min-w-[700px] h-full rounded-[15px] bg-gray-100  mt-4" >
       <div className='ml-5  mt-4 h-32 object-contain'>
