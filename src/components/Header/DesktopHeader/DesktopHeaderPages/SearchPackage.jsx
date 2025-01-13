63f4a662c0c339d80c2fff881ffbf3fdaa731177
rakesh4stepsdigital <rakesh@4stepsdigital.com>
2025-01-13 12:59:34 +0530

    calender strip

--- src/components/Header/DesktopHeader/DesktopHeaderPages/SearchPackage.jsx
+++ src/components/Header/DesktopHeader/DesktopHeaderPages/SearchPackage.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faXmark, faCalendarDays } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 import { useCarPopupContext } from "@/components/admin/context/CarPopupCalculation";
 import Link from "next/link";
@@ -32,46 +32,35 @@ const SearchPackage = ({ setSearchPackagePopup }) => {
                             searchedData?.map((pac, i) => {
                                 return (
                                     <div className="w-48 h-52">
-                                        <div className="w-full h-[70%]">
+                                        <div className="relative w-full h-[70%]">
                                             <Image
                                                 className="w-full h-full object-cover rounded-md"
-                                                src={pac?.uploads?.[0]}
-                                                alt={pac.title}
+                                                src={pac?.uploads?.[0] || '/logo.png'}
+                                                alt={"ui/ux review check"}
                                                 width={200}
                                                 height={200}
+                                                onError={(e) => {
+                                                    e.target.onerror = null;
+                                                    e.target.src = "/logo.png";
+                                                }}
                                             />
-                                        </div>
-                                        <div className="w-full h-[30%] px-4 py-1 flex flex-col justify-around items-center">
-                                            <div className="flex  justify-between h-[50%] w-full">
-                                                <p className="text-para flex gap-1 items-center">
-                                                    <Image
-                                                        width={100}
-                                                        height={100}
-                                                        className="w-3"
-                                                        src="https://cdn.iconscout.com/icon/free/png-512/free-calendar-1439790-1214120.png?f=webp&w=256"
-                                                        alt=""
-                                                    />
-                                                    {pac?.days} {"Days"}
-                                                </p>
-                                                <p className="text-para font-medium flex items-center gap-1.5 font-sans">
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        viewBox="0 0 24 24"
-                                                        fill="currentColor"
-                                                        className="-mt-0.5 h-4 w-4 md:h-5 md:w-5 text-yellow-700"
-                                                    >
-                                                        <path
-                                                            fillRule="evenodd"
-                                                            d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                                                            clipRule="evenodd"
-                                                        ></path>
-                                                    </svg>
-                                                    {pac?.packageRating}
-                                                </p>
+                                            <div className="absolute top-2">
+                                                <div className="py-1 text-xxs text-white flex gap-1 items-center rounded-r-md pl-1 pr-2 bg-black">
+                                                    {/* <FontAwesomeIcon
+                                                        icon={faCalendarDays}
+                                                        className="h-3 w-3 text-white"
+                                                    /> */}
+                                                    {pac?.days > 1 && (
+                                                        <>{pac?.days - 1} Night{pac?.days - 1 > 1 ? "s" : ""} / </>
+                                                    )}
+                                                    {pac?.days} Day{pac?.days > 1 ? "s" : ""}
+                                                </div>
                                             </div>
-                                            <h2 className="text-para font-medium h-[50%] w-full">
-                                                <Link href={"/package/"+ pac?.url + "-tour-package"}>
-                                                    {pac.name}
+                                        </div>
+                                        <div className="w-full h-[30%] px-2 py-2 flex flex-col ">
+                                            <h2 className="text-para font-medium">
+                                                <Link href={"/package/" + pac?.url + "-tour-package"}>
+                                                    {pac?.name}
                                                 </Link>
                                             </h2>
                                         </div>
