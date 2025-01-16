efea72780aed0a4556c803c1b5a2e1394837bbc6
Pradh123 <vermapradhumn3@gmail.com>
2025-01-16 12:48:17 +0530

    likujy

--- src/components/login-sinup/login/create.jsx
+++ src/components/login-sinup/login/create.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useCarPopupContext } from "@/components/admin/context/CarPopupCalculation";
-import { signIn } from "next-auth/react";
+import { signIn ,useSession} from "next-auth/react";
 import Cookies from "js-cookie";
 const Create = () => {
   const { setLoginPopup } = useCarPopupContext();
@@ -12,11 +12,12 @@ const Create = () => {
       document.body.style.overflow = "auto";
     };
   }, []);
-  
-  const [step, setStep] = useState(1); 
+  const token = Cookies.get("token");
+  const {data:session}=useSession();
+  const [step, setStep] = useState(1);
   const [phoneNumber, setPhoneNumber] = useState("");
   const [otp, setOtp] = useState("");
-  const [timer, setTimer] = useState(60); 
+  const [timer, setTimer] = useState(60);
   const [mobileError, setMobileError] = useState("");
   const [otpError, setOtpError] = useState("");
   const [resendOtpError, setResendOtpError] = useState("");
@@ -92,8 +93,9 @@ const Create = () => {
         });
         const data = await response.json();
         if (data.success) {
-          Cookies.set("token",data?.data?.token,{expires:1,secure:true})
-          setLoginPopup(false);
+          Cookies.set("token", data?.data?.token, { expires: 1, secure: true });
+          // setLoginPopup(true);
+          setStep(1);
           setPhoneNumber("");
           setOtp("");
           alert("OTP verified successfully!");
@@ -107,7 +109,7 @@ const Create = () => {
     }
   };
 
-  const handleResendOtp = async() => {
+  const handleResendOtp = async () => {
     if (timer === 0) {
       const number = "+91" + phoneNumber;
       if (!/^\+\d{12}$/.test(number)) {
@@ -134,18 +136,21 @@ const Create = () => {
         console.error("Error sending OTP:", error);
         alert("An error occurred. Please try again.");
       }
-     
     } else {
       setResendOtpError("Please wait before resending the OTP.");
     }
   };
- 
+
+  const handleMobileVerifiedAndGetMoreDetail=()=>{
+    
+    setLoginPopup(false);
+  }
 
   return (
     <>
       <div className="fixed inset-0 flex items-center h-[100vh] justify-center z-50">
         <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="bg-white rounded-xl shadow-lg z-50 w-80 h-[470px] max-h-[500px]">
+        <div className="bg-white rounded-xl shadow-lg z-50 w-80 h-auto max-h-[500px]">
           {step === 1 ? (
             <div>
               <div className="pr-2 pt-1 flex justify-end items-center mt-1">
@@ -170,56 +175,82 @@ const Create = () => {
                     alt=""
                   />
                 </div>
-                <div className="flex flex-col justify-center mt-5">
-                  <p className="text-center mb-3 font-medium capitalize">
-                    Create an Account
-                  </p>
-                  <div
-                    tabindex="1"
-                    className="flex w-full px-1 py-2 border border-gray-300 rounded-full focus-within:outline-none focus-within: ring-0 focus-within:ring-orange-400 focus-within:border-orange-400"
-                  >
-                    <p className=" border-r px-1 border-grey-500">+91</p>
-                    <input
-                      type="text"
-                      value={phoneNumber}
-                      onChange={(e) => {
-                        setPhoneNumber(e.target.value);
-                        validateMobile(e.target.value);
-                      }}
-                      placeholder="XXXXXXXXXX"
-                      className=" outline-none px-1"
-                      required
-                    />
+                {(token===undefined&&session===null)&&<div>
+                  <div className="flex flex-col justify-center mt-5">
+                    <p className="text-center mb-3 font-medium capitalize">
+                      Create an Account
+                    </p>
+                    <div
+                      tabindex="1"
+                      className="flex w-full px-1 py-2 border border-gray-300 rounded-full focus-within:outline-none focus-within: ring-0 focus-within:ring-orange-400 focus-within:border-orange-400"
+                    >
+                      <p className=" border-r px-1 border-grey-500">+91</p>
+                      <input
+                        type="text"
+                        value={phoneNumber}
+                        onChange={(e) => {
+                          setPhoneNumber(e.target.value);
+                          validateMobile(e.target.value);
+                        }}
+                        placeholder="XXXXXXXXXX"
+                        className=" outline-none px-1"
+                        required
+                      />
+                    </div>
                   </div>
-                </div>
-                {mobileError && (
-                  <p className="text-red-500 text-xxs text-center">
-                    {mobileError}
-                  </p>
-                )}
-                <div className="flex flex-col justify-center items-center my-5">
-                  <button
-                    onClick={handlePhoneSubmit}
-                    className="w-full bg-navyblack text-white px-5 py-2 rounded-full"
-                  >
-                    Continue
-                  </button>
-                </div>
-                <div class="flex items-center justify-between my-5">
-                  <span class="border-b w-1/5 lg:w-1/4 ml-2"></span>
-                  <p class="text-xxs text-center text-gray-500 uppercase">
-                    or continue with
-                  </p>
-                  <span class="border-b w-1/5 lg:w-1/4 mr-2"></span>
-                </div>
-                <div className="flex justify-center my-5">
-                  <button
-                    onClick={() => signIn("google")}
-                    className="w-full bg-gray-200 text-navyblack px-5 py-2 rounded-full"
-                  >
-                    Google
-                  </button>
-                </div>
+                  {mobileError && (
+                    <p className="text-red-500 text-xxs text-center">
+                      {mobileError}
+                    </p>
+                  )}
+                  <div className="flex flex-col justify-center items-center my-5">
+                    <button
+                      onClick={handlePhoneSubmit}
+                      className="w-full bg-navyblack text-white px-5 py-2 rounded-full"
+                    >
+                      Continue
+                    </button>
+                  </div>
+                  <div class="flex items-center justify-between my-5">
+                    <span class="border-b w-1/5 lg:w-1/4 ml-2"></span>
+                    <p class="text-xxs text-center text-gray-500 uppercase">
+                      or continue with
+                    </p>
+                    <span class="border-b w-1/5 lg:w-1/4 mr-2"></span>
+                  </div>
+                  <div className="flex justify-center my-5">
+                    <button
+                      onClick={() => signIn("google")}
+                      className="w-full bg-gray-200 text-navyblack px-5 py-2 rounded-full"
+                    >
+                      Google
+                    </button>
+                  </div>
+                </div>}
+                {token!==undefined&&<div className="flex flex-col gap-2">
+                  <h1 className="text-sm font-medium text-center"> Provide Details</h1>
+                  <div className="flex flex-col ">
+                    <label htmlFor="firstname" className="text-sm">First Name</label>
+                    <input type="text" id="firstname" placeholder="Enter First Name" required className="text-sm outline-none flex w-full px-1 py-1.5 border border-gray-300 rounded-md focus-within:outline-none focus-within: ring-0 focus-within:ring-orange-400 focus-within:border-orange-400 " />
+                  </div>
+                  <div className="flex flex-col">
+                    <label htmlFor="lastname" className="text-sm ">Last Name</label>
+                    <input type="text"  id="lastname" placeholder="Enter Last Name"  className="text-sm outline-none flex w-full px-1 py-1.5 border border-gray-300 rounded-md focus-within:outline-none focus-within: ring-0 focus-within:ring-orange-400 focus-within:border-orange-400 "/>
+                  </div>
+                  <div className="flex flex-col">
+                    <label htmlFor="email" required className="text-sm">Email</label>
+                    <input type="email"  id="email" placeholder="Enter Email Address"  className=" text-sm outline-none flex w-full px-1 py-1.5 border border-gray-300 rounded-md focus-within:outline-none focus-within: ring-0 focus-within:ring-orange-400 focus-within:border-orange-400 "/>
+                  </div>
+                  <button onClick={handleMobileVerifiedAndGetMoreDetail} className="w-full mb-4 bg-navyblack text-white px-5 py-2 rounded-full mt-2.5">Proceed</button>
+                  </div>}
+                  {session!==null&&<div className="flex flex-col gap-2">
+                  <h1 className="text-para font-semibold text-center"> Provide Details</h1>
+                  <div className="flex flex-col mt-3 ">
+                    <label htmlFor="firstname" className="text-sm">Mobile No.</label>
+                    <input type="text" id="firstname" placeholder="Enter Mobile No." required className="text-sm outline-none flex w-full px-1 py-1.5 border border-gray-300 rounded-md focus-within:outline-none focus-within: ring-0 focus-within:ring-orange-400 focus-within:border-orange-400 " />
+                  </div>
+                  <button className="w-full bg-navyblack text-white px-5 mb-10 mt-5 py-2 rounded-full ">Proceed</button>
+                  </div>}
               </div>
             </div>
           ) : (
